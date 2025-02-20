52eb5dc71176a5e415e89ee4bec3936afec62a5c
Kyle Fred <kyle.fred.finch@gmail.com>
2025-02-20 23:06:30 +0000

    uncomment tests

--- backend/src/test/java/com/budgetmaster/BudgetMasterApplicationTests.java
+++ backend/src/test/java/com/budgetmaster/BudgetMasterApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.budgetmaster;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class BudgetMasterApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.budgetmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

033d27fe9eebdee806214489bf9addf1b26442c3
kyle-fred <kyle.fred.finch@gmail.com>
2025-03-06 15:42:58 +0000

    import ordering

--- backend/src/test/java/com/budgetmaster/controller/ExpenseControllerTest.java
+++ backend/src/test/java/com/budgetmaster/controller/ExpenseControllerTest.java
@@ -14,10 +14,10 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 

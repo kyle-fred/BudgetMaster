43e7b6b7b922d51d1605dec49ae20a1736bebe50
kyle-fred <kyle.fred.finch@gmail.com>
2025-03-31 21:59:22 +0100

    upgrade spring boot version and remove unnecessary import (#41)

--- backend/src/test/java/com/budgetmaster/controller/ExpenseControllerTest.java
+++ backend/src/test/java/com/budgetmaster/controller/ExpenseControllerTest.java
@@ -22,7 +22,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.Optional;
 

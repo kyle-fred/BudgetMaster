62aa962f66eecd7798efda4bd70c7d4644f833f2
Kyle Fred <kyle.fred.finch@gmail.com>
2025-05-20 17:51:23 +0100

    update expense constants with income constants

--- backend/src/test/java/com/budgetmaster/testsupport/expense/constants/ExpenseConstants.java
+++ backend/src/test/java/com/budgetmaster/testsupport/expense/constants/ExpenseConstants.java
@@ -49,7 +49,7 @@ public final class ExpenseConstants {
     public static final class NonExistent {
         private NonExistent() {}
 
-        public static final Long ID = 999L;
+        public static final Long ID = 999999L;
         public static final int YEAR = 3000;
         public static final int MONTH = 1;
         public static final YearMonth YEAR_MONTH = YearMonth.of(YEAR, MONTH);

fe5d7ca9dec61b73fa88ac4fa58a82a7ddb94f89
Kyle Fred <kyle.fred.finch@gmail.com>
2025-06-05 21:16:33 +0100

    income controller test refactored to use assertion helper class

---
+++ backend/src/test/java/com/budgetmaster/testsupport/assertions/controller/IncomeControllerAssertions.java
@@ -0,0 +1,112 @@
+package com.budgetmaster.testsupport.assertions.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.YearMonth;
+
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.budgetmaster.application.enums.TransactionType;
+import com.budgetmaster.application.model.Money;
+import com.budgetmaster.testsupport.assertions.controller.error.ErrorControllerAssertions;
+import com.budgetmaster.testsupport.assertions.model.MoneyAssertions;
+import com.budgetmaster.testsupport.builder.model.MoneyBuilder;
+import com.budgetmaster.testsupport.constants.ErrorConstants;
+import com.budgetmaster.testsupport.constants.PathConstants;
+import com.budgetmaster.testsupport.constants.domain.IncomeConstants;
+
+public class IncomeControllerAssertions {
+
+    private final ResultActions resultActions;
+
+    private IncomeControllerAssertions(ResultActions resultActions) {
+        this.resultActions = resultActions;
+    }
+
+    public static IncomeControllerAssertions assertThat(ResultActions resultActions) {
+        return new IncomeControllerAssertions(resultActions);
+    }
+
+    public IncomeControllerAssertions isOk() throws Exception {
+        resultActions.andExpect(status().isOk());
+        return this;
+    }
+
+    public IncomeControllerAssertions isNoContent() throws Exception {
+        resultActions.andExpect(status().isNoContent());
+        return this;
+    }
+
+    public IncomeControllerAssertions hasName(String expectedName) throws Exception {
+        resultActions.andExpect(jsonPath(PathConstants.JsonProperties.Income.NAME).value(expectedName));
+        return this;
+    }
+
+    public IncomeControllerAssertions hasSource(String expectedSource) throws Exception {
+        resultActions.andExpect(jsonPath(PathConstants.JsonProperties.Income.SOURCE).value(expectedSource));
+        return this;
+    }
+
+    public IncomeControllerAssertions hasMoney(Money expectedMoney) throws Exception {
+        MoneyAssertions.assertThat(resultActions)
+            .hasMoney(expectedMoney);
+        return this;
+    }
+
+    public IncomeControllerAssertions hasType(TransactionType expectedType) throws Exception {
+        resultActions.andExpect(jsonPath(PathConstants.JsonProperties.Income.TYPE).value(expectedType.toString()));
+        return this;
+    }
+
+    public IncomeControllerAssertions hasMonth(YearMonth expectedMonth) throws Exception {
+        resultActions
+            .andExpect(jsonPath(PathConstants.JsonProperties.Income.MONTH_YEAR).isArray())
+            .andExpect(jsonPath(PathConstants.JsonProperties.Income.MONTH).value(expectedMonth.getMonthValue()))
+            .andExpect(jsonPath(PathConstants.JsonProperties.Income.YEAR).value(expectedMonth.getYear()));
+        return this;
+    }
+
+    public IncomeControllerAssertions isDefaultIncomeResponse() throws Exception {
+        return isOk()
+            .hasName(IncomeConstants.Default.NAME)
+            .hasSource(IncomeConstants.Default.SOURCE)
+            .hasMoney(MoneyBuilder.defaultIncome().build())
+            .hasType(IncomeConstants.Default.TYPE)
+            .hasMonth(IncomeConstants.Default.YEAR_MONTH);
+    }
+
+    public IncomeControllerAssertions isUpdatedIncomeResponse() throws Exception {
+        return isOk()
+            .hasName(IncomeConstants.Updated.NAME)
+            .hasSource(IncomeConstants.Updated.SOURCE)
+            .hasMoney(MoneyBuilder.updatedMoney().build())
+            .hasType(IncomeConstants.Updated.TYPE)
+            .hasMonth(IncomeConstants.Updated.YEAR_MONTH);
+    }
+
+    public ErrorControllerAssertions isNotFoundForMonth(YearMonth month) throws Exception {
+        return ErrorControllerAssertions.assertThat(resultActions)
+            .isNotFoundResponse(
+                String.format(ErrorConstants.Income.NOT_FOUND_BY_MONTH, month),
+                PathConstants.Error.Income.URI
+            );
+    }
+
+    public ErrorControllerAssertions isNotFoundForId(Long id) throws Exception {
+        return ErrorControllerAssertions.assertThat(resultActions)
+            .isNotFoundResponse(
+                String.format(ErrorConstants.Income.NOT_FOUND_WITH_ID, id),
+                String.format(PathConstants.Error.Income.URI_WITH_ID, id)
+            );
+    }
+
+    public ErrorControllerAssertions isInternalServerError() throws Exception {
+        return ErrorControllerAssertions.assertThat(resultActions)
+            .isInternalServerErrorResponse(PathConstants.Error.Income.URI);
+    }
+
+    public ErrorControllerAssertions isConflict() throws Exception {
+        return ErrorControllerAssertions.assertThat(resultActions)
+            .isConflictResponse(PathConstants.Error.Income.URI);
+    }
+}

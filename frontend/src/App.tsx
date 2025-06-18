1edad45127f68bff94cd64124f95f0f2dcdd76d7
Kyle Fred <kyle.fred.finch@gmail.com>
2025-06-18 18:50:15 +0100

    hello world working!!

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>BudgetMaster</h1>
+      <p>Hello World! Welcome to your budget management app.</p>
+    </div>
+  )
+}
+
+export default App
